0cc72fd2d02e2562822e9bfaafb35b9860659764
Binti Cahayati <binti.cahayati@ninjavan.co>
2022-01-24 11:49:36 +0700

    add rts value

--- src/test/java/co/nvqa/core_api/cucumber/glue/features/DriverSteps.java
+++ src/test/java/co/nvqa/core_api/cucumber/glue/features/DriverSteps.java
@@ -229,6 +229,7 @@ public class DriverSteps extends BaseSteps {
     job.setAction(action);
     job.setParcelWeight(order.getParcelWeight());
     job.setShipperId(order.getShipperId());
+    job.setRts(order.getRts());
     if (action.equalsIgnoreCase(Job.ACTION_FAIL)) {
       boolean idValidFailed = get(KEY_BOOLEAN_DRIVER_FAILED_VALID, false);
       if (idValidFailed) {
