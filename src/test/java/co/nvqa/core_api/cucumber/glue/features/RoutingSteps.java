75d4375c2ad8d19bbf2acd9cecb05d252b3ead88
Binti Cahayati <binti.cahayati@ninjavan.co>
2022-04-07 11:15:43 +0700

    add zonal routing scenarios

--- src/test/java/co/nvqa/core_api/cucumber/glue/features/RoutingSteps.java
+++ src/test/java/co/nvqa/core_api/cucumber/glue/features/RoutingSteps.java
@@ -66,135 +66,6 @@ public class RoutingSteps extends BaseSteps {
     }, "create empty route");
   }
 
-  @When("Operator create a route and assign waypoint from Zonal Routing API")
-  public void operatorCreateRouteZr(Map<String, String> arg1) {
-    final String json = toJsonCamelCase(arg1);
-    final ZonalRoutingRouteRequest route = fromJsonSnakeCase(json, ZonalRoutingRouteRequest.class);
-    final List<Long> waypointIds = get(KEY_LIST_OF_WAYPOINT_IDS);
-    route.setTags(Arrays.asList(1, 4));
-    route.setDate(generateUTCTodayDate());
-    route.setWaypoints(waypointIds);
-    callWithRetry(() -> {
-      List<Route> result = getRouteClient()
-          .zonalRoutingCreateRoute(Collections.singletonList(route));
-      Assertions.assertThat(result.get(0)).as("created route is not null").isNotNull();
-      put(KEY_CREATED_ROUTE, result);
-      putInList(KEY_LIST_OF_CREATED_ROUTE_ID, result.get(0).getId());
-      put(KEY_CREATED_ROUTE_ID, result.get(0).getId());
-      put(RoutingSteps.KEY_ROUTE_EVENT_SOURCE, "ZONAL_ROUTING_CREATE");
-    }, "zonal routing create route");
-  }
-
-  //add unrouted waypoints & edit waypoint sequence
-  @When("Operator edit route from Zonal Routing API")
-  public void operatorEditRouteZr(Map<String, String> arg1) {
-    final Long routeId = get(KEY_CREATED_ROUTE_ID);
-    final String json = toJsonCamelCase(arg1);
-    final ZonalRoutingRouteRequest route = fromJsonSnakeCase(json, ZonalRoutingRouteRequest.class);
-    final List<Long> waypointIds = get(KEY_LIST_OF_WAYPOINT_IDS);
-    final List<Long> orderIds = get(KEY_LIST_OF_CREATED_ORDER_ID);
-    if (arg1.containsKey("to_edit_sequence")) {
-      Collections.shuffle(waypointIds);
-    }
-    route.setTags(Arrays.asList(1, 4));
-    route.setWaypoints(waypointIds);
-    route.setId(routeId);
-    put(KEY_LIST_OF_WAYPOINTS_SEQUENCE, waypointIds);
-    callWithRetry(() -> {
-      List<Route> result = getRouteClient()
-          .zonalRoutingEditRoute(Collections.singletonList(route));
-      Assertions.assertThat(result.get(0)).as("updated route is not null").isNotNull();
-      remove(KEY_LIST_OF_CREATED_ORDER_ID);
-      orderIds.remove(0);
-      putAllInList(KEY_LIST_OF_CREATED_ORDER_ID, orderIds);
-      put(RoutingSteps.KEY_ROUTE_EVENT_SOURCE, "ZONAL_ROUTING_UPDATE");
-    }, "zonal routing edit route");
-  }
-
-  //remove waypoints
-  @When("Operator edit route by removing waypoints from Zonal Routing API")
-  public void operatorEditRouteRemoveZr(Map<String, String> arg1) {
-    final long routeId = get(KEY_CREATED_ROUTE_ID);
-    final String json = toJsonCamelCase(arg1);
-    final ZonalRoutingRouteRequest route = fromJsonSnakeCase(json, ZonalRoutingRouteRequest.class);
-    final Long waypointId = get(KEY_WAYPOINT_ID);
-    final Long transactionId = get(KEY_TRANSACTION_ID);
-    final Long orderId = get(KEY_CREATED_ORDER_ID);
-    final List<Long> waypointIds = get(KEY_LIST_OF_WAYPOINT_IDS);
-    final List<Long> transactionIds = get(KEY_LIST_OF_TRANSACTION_IDS);
-    final List<Long> orderIds = get(KEY_LIST_OF_CREATED_ORDER_ID);
-    route.setTags(Arrays.asList(1, 4));
-    route.setWaypoints(Collections.singletonList(waypointId));
-    route.setId(routeId);
-    callWithRetry(() -> {
-      List<Route> result = getRouteClient()
-          .zonalRoutingEditRoute(Collections.singletonList(route));
-      Assertions.assertThat(result.get(0)).as("updated route is not null").isNotNull();
-      put(RoutingSteps.KEY_ROUTE_EVENT_SOURCE, "ZONAL_ROUTING_UPDATE");
-      //remove all waypoints, transactions, orders from map
-      remove(KEY_LIST_OF_WAYPOINT_IDS);
-      remove(KEY_LIST_OF_TRANSACTION_IDS);
-      remove(KEY_LIST_OF_CREATED_ORDER_ID);
-      //include only removed waypoints, transactions, orders
-      waypointIds.remove(waypointId);
-      transactionIds.remove(transactionId);
-      orderIds.remove(orderId);
-      //add only removed waypoints, transactions, orders
-      putAllInList(KEY_LIST_OF_WAYPOINT_IDS, waypointIds);
-      putAllInList(KEY_LIST_OF_TRANSACTION_IDS, transactionIds);
-      putAllInList(KEY_LIST_OF_CREATED_ORDER_ID, orderIds);
-      putAllInList(KEY_LIST_OF_REMAINING_WAYPOINT_IDS, route.getWaypoints());
-      putAllInList(KEY_LIST_OF_REMOVED_WAYPOINT_IDS, waypointIds);
-    }, "zonal routing edit route");
-  }
-
-  //move routed waypoints to another route
-  @When("Operator edit route by moving to another route from Zonal Routing API")
-  public void operatorEditRouteMoveToAnotherRouteZr(Map<String, String> arg1) {
-    final List<Long> routeIds = get(KEY_LIST_OF_CREATED_ROUTE_ID);
-    final String json = toJsonCamelCase(arg1);
-    final Long waypointId = get(KEY_WAYPOINT_ID);
-    final Long transactionId = get(KEY_TRANSACTION_ID);
-    final Long orderId = get(KEY_CREATED_ORDER_ID);
-    final List<Long> waypointIds = get(KEY_LIST_OF_WAYPOINT_IDS);
-    final List<Long> transactionIds = get(KEY_LIST_OF_TRANSACTION_IDS);
-    final List<Long> orderIds = get(KEY_LIST_OF_CREATED_ORDER_ID);
-
-    List<ZonalRoutingRouteRequest> request = new ArrayList<>();
-    //removed from route
-    ZonalRoutingRouteRequest removedRoute = fromJson(json, ZonalRoutingRouteRequest.class);
-    removedRoute.setTags(Arrays.asList(1, 4));
-    removedRoute.setWaypoints(Collections.singletonList(waypointId));
-    removedRoute.setId(routeIds.get(0));
-    request.add(removedRoute);
-
-    //moved to new route
-    ZonalRoutingRouteRequest movedToRoute = fromJson(json, ZonalRoutingRouteRequest.class);
-    movedToRoute.setTags(Arrays.asList(1, 4));
-    waypointIds.remove(waypointId);
-    movedToRoute.setWaypoints(waypointIds);
-    movedToRoute.setId(routeIds.get(1));
-    request.add(movedToRoute);
-
-    callWithRetry(() -> {
-      List<Route> result = getRouteClient()
-          .zonalRoutingEditRoute(request);
-      Assertions.assertThat(result.get(0)).as("removed route is not null").isNotNull();
-      Assertions.assertThat(result.get(1)).as("moved to route is not null").isNotNull();
-      put(RoutingSteps.KEY_ROUTE_EVENT_SOURCE, "ZONAL_ROUTING_UPDATE");
-      remove(KEY_LIST_OF_WAYPOINT_IDS);
-      remove(KEY_LIST_OF_TRANSACTION_IDS);
-      remove(KEY_LIST_OF_CREATED_ORDER_ID);
-      orderIds.remove(orderId);
-      waypointIds.remove(waypointId);
-      transactionIds.remove(transactionId);
-      putAllInList(KEY_LIST_OF_WAYPOINT_IDS, waypointIds);
-      putAllInList(KEY_LIST_OF_TRANSACTION_IDS, transactionIds);
-      putAllInList(KEY_LIST_OF_CREATED_ORDER_ID, orderIds);
-      put(KEY_LIST_ZONAL_ROUTING_EDIT_ROUTE, request);
-    }, "zonal routing edit route move to another route");
-  }
-
   @When("Operator create an empty route with past date")
   public void operatorCreateEmptyRoutePastDate(Map<String, String> arg1) {
     final String json = toJsonCamelCase(arg1);
@@ -427,7 +298,7 @@ public class RoutingSteps extends BaseSteps {
     }, "verify archive driver route v2");
   }
 
-  private String generateUTCTodayDate() {
+  public static String generateUTCTodayDate() {
     ZonedDateTime startDateTime = DateUtil.getStartOfDay(DateUtil.getDate());
     return DateUtil
         .displayDateTime(startDateTime.withZoneSameInstant(ZoneId.of("UTC")));
